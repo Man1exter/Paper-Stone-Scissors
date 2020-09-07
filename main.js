e02099c3fdf5ca8842e6276a0460bb094625ea9e
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-07 10:27:40 +0200

    add little corrects

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function ourWays(){
  ways.forEach(way => way.style.boxShadow = '');
  ways.forEach(way => way.style.margin = '');
  ways.forEach(way => way.style.backgroundColor = '');
- this.style.boxShadow = "6px 3px 23px 30px rgb(1, 253, 241)";
+ this.style.boxShadow = "6px 3px 23px 30px rgb(1, 252, 241)";
  this.style.margin = "50px 0px 20px 0px";
  this.style.backgroundColor = "white";
 }
