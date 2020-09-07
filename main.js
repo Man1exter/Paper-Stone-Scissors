c75d1a2f0004e828bc93085493c2ac962d395d55
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-07 10:25:26 +0200

    complete click images

--- main.js
+++ main.js
@@ -5,16 +5,20 @@ const gameSum = {
 }
 
 const game = {
-    playerWays = '',
-    cpuWays = '',
+    playerWays:  "",
+    cpuWays:  "",
 }
 
 const ways = [...document.querySelectorAll('.images img')]; //every selected images 
 
 function ourWays(){
  game.playerWays = this.dataset.option;
- ways.forEach(way = way.style.boxShadow = '');
+ ways.forEach(way => way.style.boxShadow = '');
+ ways.forEach(way => way.style.margin = '');
+ ways.forEach(way => way.style.backgroundColor = '');
  this.style.boxShadow = "6px 3px 23px 30px rgb(1, 253, 241)";
+ this.style.margin = "50px 0px 20px 0px";
+ this.style.backgroundColor = "white";
 }
 
 ways.forEach(way => way.addEventListener("click", ourWays));
