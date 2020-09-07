5a15642ce4e51c24c34e6f86164d421fc272b17b
Mariusz Perzynski <m.perzyk@interia.pl>
2020-09-07 10:18:23 +0200

    add ways for each and box shadow after to click fc

--- main.js
+++ main.js
@@ -13,8 +13,8 @@ const ways = [...document.querySelectorAll('.images img')]; //every selected ima
 
 function ourWays(){
  game.playerWays = this.dataset.option;
+ ways.forEach(way = way.style.boxShadow = '');
+ this.style.boxShadow = "6px 3px 23px 30px rgb(1, 253, 241)";
 }
-
-
 
 ways.forEach(way => way.addEventListener("click", ourWays));
